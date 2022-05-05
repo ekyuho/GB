e2351ca07082567200f1616db160338a06badd4d
Kyuho Kim <ekyuho@gmail.com>
2022-05-05 15:41:05 +0900

    stage2

--- Client_Data_Saving.py
+++ Client_Data_Saving.py
@@ -17,64 +17,26 @@ import time
 from datetime import datetime
 from paho.mqtt import client as mqtt
 
-#host="m.damoa.io"
-host="218.232.234.232"  #건교부 테스트 사이트
-cse={'name':'cse-gnrb-mon'}
-bridge='025742'  #교량코드
-ae={}
-cnt_proto={'config':{'ctrigger':{},'time':{},'cmeasure':{},'connect':{}},
-           'info':{'manufacture':{}, 'install':{},'imeasure':{}},
-           'data':{'dtrigger':{},'fft':{},'dmeasure':{}},
-           'state':{},
-           'ctrl':{"sub":1}}
-ae[F'ae.{bridge}-AC_A1_01_X']={'name':'Accelerator', 'cnt':cnt_proto}
-ae[F'ae.{bridge}-AC_A1_02_X']={'name':'Accelerator', 'cnt':cnt_proto}
-ae[F'ae.{bridge}-AC_A1_03_X']={'name':'Accelerator', 'cnt':cnt_proto}
-ae[F'ae.{bridge}-DI_A1_01_X']={'name':'Displacement Guage', 'cnt':cnt_proto}
-ae[F'ae.{bridge}-TP_A1_01_X']={'name':'Temperature', 'cnt':cnt_proto}
-ae[F'ae.{bridge}-TI_A1_01_X']={'name':'Inclinometer', 'cnt':cnt_proto}
-#기본적인 ae 데이터 입력
+import conf
+broker = conf.host
+port = conf.port
+bridge = conf.bridge
+cse = conf.cse
+ae = conf.ae
 
-broker = host
-port = 1883
+root=conf.root
 
 # mqtt server TOPIC
-TOPIC_acc1 = F'/{cse["name"]}/ae.{bridge}-AC_A1_01_X/realtime'
-TOPIC_acc2 = F'/{cse["name"]}/ae.{bridge}-AC_A1_02_X/realtime'
-TOPIC_acc3 = F'/{cse["name"]}/ae.{bridge}-AC_A1_03_X/realtime'
-TOPIC_dis = F'/{cse["name"]}/ae.{bridge}-DI_A1_01_X/realtime'
-TOPIC_tem = F'/{cse["name"]}/ae.{bridge}-TP_A1_01_X/realtime'
-TOPIC_deg = F'/{cse["name"]}/ae.{bridge}-TI_A1_01_X/realtime'
+TOPIC_acc1 = conf.TOPIC_acc1
+TOPIC_acc2 = conf.TOPIC_acc2
+TOPIC_acc3 = conf.TOPIC_acc3
+TOPIC_dis = conf.TOPIC_dis
+TOPIC_tem = conf.TOPIC_tem
+TOPIC_deg = conf.TOPIC_deg
 
-# mqtt를 사용할 것인지, 그렇다면 어느 센서의 데이터를 보낼지에 대한 딕셔너리
-mqtt_list = {
-    "use":"Y",
-    "acc1":"Y",
-    "acc2":"N",
-    "acc3":"N",
-    "dis":"N",
-    "tem":"N",
-    "deg":"N"
-    }
-
-# mqtt 데이터에 보낼 samplerate list. 정적 데이터의 경우 1초에 1개를 보내기 때문에 1, 동적 데이터의 경우 1초에 들어오는 데이터 개수만큼의 samplerate를 설정해두었다
-samplerate_list = {
-    "acc1":100,
-    "acc2":100,
-    "acc3":100,
-    "dis":1,
-    "tem":1,
-    "deg":1
-    }
-
-TOPIC_list = {
-    "acc1":TOPIC_acc1,
-    "acc2":TOPIC_acc2,
-    "acc3":TOPIC_acc3,
-    "dis":TOPIC_dis,
-    "tem":TOPIC_tem,
-    "deg":TOPIC_deg
-    }
+mqtt_list = conf.mqtt_list
+samplerate_list = conf.samplerate_list
+TOPIC_list = conf.TOPIC_list
 
 # 다중 데이터의 경우, 어떤 data를 저장할지 결정해야한다
 acc_axis = "x" # x, y, z중 택1
@@ -82,12 +44,18 @@ deg_axis = "x" # x, y, z중 택1
 str_axis = "z" # x, y, z중 택1
 dis_channel = "ch4" # ch4, ch5중 택1
 
-#센서 별 데이터를 저장할 디렉토리 루트. 디렉토리가 미리 생성되어있지 않으면 에러메시지가 뜰 것으로 예상됨.
-acc_path = "./raw_data/Acceleration"
-deg_path = "./raw_data/Degree"
-dis_path = "./raw_data/Displacement"
-str_path = "./raw_data/Strain"
-tem_path = "./raw_data/Temperature"
+#센서별 데이타 저장소, 디렉토리가 없으면 자동생성
+acc_path = F"{root}/raw_data/Acceleration"
+deg_path = F"{root}/raw_data/Degree"
+dis_path = F"{root}/raw_data/Displacement"
+str_path = F"{root}/raw_data/Strain"
+tem_path = F"{root}/raw_data/Temperature"
+if not os.path.exists(F"{root}/raw_data"): os.makedirs(F"{root}/raw_data")
+if not os.path.exists(acc_path): os.makedirs(acc_path)
+if not os.path.exists(deg_path): os.makedirs(deg_path)
+if not os.path.exists(dis_path): os.makedirs(dis_path)
+if not os.path.exists(str_path): os.makedirs(str_path)
+if not os.path.exists(tem_path): os.makedirs(tem_path)
 
 # dict jsonCreate(dataTyep, timeData, realData)
 # 받은 인자를 통해 딕셔너리를 생성합니다.
@@ -137,12 +105,12 @@ if mqtt_list["use"] == "Y":
     mqttc.connect(broker, port)
 
 while True:
-    ClientSock.sendall("CAPTURE".encode()) # 1초에 1번 socket server로 'CAPTURE' 명령어를 송신합니다.
+    ClientSock.sendall("CAPTURE".encode()) # 0.9초에 1번 socket server로 'CAPTURE' 명령어를 송신합니다.
     jsonData = ClientSock.recv(10000)
     jsonData = jsonData.decode('utf_8')
     jsonData = json.loads(jsonData) # jsonData : 서버로부터 받은 json file을 dict 형식으로 변환한 것
     if jsonData["Status"] == "False":
-        print("data isn't ready yet")
+        print("** no data", datetime.now().strftime("(%Y-%m-%d %H:%M:%S)"))
     else:
         Time_data = jsonData["Timestamp"]
         Temperature_data = jsonData["Temperature"]
@@ -180,6 +148,8 @@ while True:
         jsonSave(dis_path, Displacement_json)
         jsonSave(acc_path, Acceleration_json)
         jsonSave(str_path, Strain_json)
+	
+        print("data ok", datetime.now().strftime("(%Y-%m-%d %H:%M:%S)"))
 
     #명령은 약 0.9초에 1번 보낸다            
     time.sleep(0.9)
---
+++ conf.py
@@ -0,0 +1,58 @@
+#host="m.damoa.io"
+host="218.232.234.232"  #건교부 테스트 사이트
+port=1883
+
+cse={'name':'cse-gnrb-mon'}
+bridge='025742'  #교량코드
+ae={}
+cnt_proto={'config':{'ctrigger':{},'time':{},'cmeasure':{},'connect':{}},
+           'info':{'manufacture':{}, 'install':{},'imeasure':{}},
+           'data':{'dtrigger':{},'fft':{},'dmeasure':{}},
+           'state':{},
+           'ctrl':{"sub":1}}
+ae[F'ae.{bridge}-AC_A1_01_X']={'name':'Accelerator', 'cnt':cnt_proto}
+ae[F'ae.{bridge}-AC_A1_02_X']={'name':'Accelerator', 'cnt':cnt_proto}
+ae[F'ae.{bridge}-AC_A1_03_X']={'name':'Accelerator', 'cnt':cnt_proto}
+ae[F'ae.{bridge}-DI_A1_01_X']={'name':'Displacement Guage', 'cnt':cnt_proto}
+ae[F'ae.{bridge}-TP_A1_01_X']={'name':'Temperature', 'cnt':cnt_proto}
+ae[F'ae.{bridge}-TI_A1_01_X']={'name':'Inclinometer', 'cnt':cnt_proto}
+
+root='/home/pi/GB'
+
+# mqtt server TOPIC
+TOPIC_acc1 = F'/{cse["name"]}/ae.{bridge}-AC_A1_01_X/realtime'
+TOPIC_acc2 = F'/{cse["name"]}/ae.{bridge}-AC_A1_02_X/realtime'
+TOPIC_acc3 = F'/{cse["name"]}/ae.{bridge}-AC_A1_03_X/realtime'
+TOPIC_dis = F'/{cse["name"]}/ae.{bridge}-DI_A1_01_X/realtime'
+TOPIC_tem = F'/{cse["name"]}/ae.{bridge}-TP_A1_01_X/realtime'
+TOPIC_deg = F'/{cse["name"]}/ae.{bridge}-TI_A1_01_X/realtime'
+
+# mqtt를 사용할 것인지, 그렇다면 어느 센서의 데이터를 보낼지에 대한 딕셔너리
+mqtt_list = {
+    "use":"Y",
+    "acc1":"Y",
+    "acc2":"N",
+    "acc3":"N",
+    "dis":"N",
+    "tem":"N",
+    "deg":"N"
+    }
+
+# mqtt 데이터에 보낼 samplerate list. 정적 데이터의 경우 1초에 1개를 보내기 때문에 1, 동적 데이터의 경우 1초에 들어오는 데이터 개수만큼의 samplerate를 설정해두었다
+samplerate_list = {
+    "acc1":100,
+    "acc2":100,
+    "acc3":100,
+    "dis":1,
+    "tem":1,
+    "deg":1
+    }
+
+TOPIC_list = {
+    "acc1":TOPIC_acc1,
+    "acc2":TOPIC_acc2,
+    "acc3":TOPIC_acc3,
+    "dis":TOPIC_dis,
+    "tem":TOPIC_tem,
+    "deg":TOPIC_deg
+    }
