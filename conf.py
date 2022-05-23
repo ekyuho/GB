dc527b42b2fa528d022b2466e6527306f17b6f1a
joosa <joo1798@naver.com>
2022-05-23 16:28:41 +0900

    건교부 호스트로 변경

--- conf.py
+++ conf.py
@@ -5,25 +5,25 @@ import json
 # ctrl command 로 reset 을 실행하거나, config.dat 를 삭제하면 다시 아래값을 1회 읽어간다.
 
 csename='cse-gnrb-mon'
-#host="218.232.234.232"  #건교부 테스트 사이트
-host="m.damoa.io"  #이노온 테스트 사이트
+host="218.232.234.232"  #건교부 테스트 사이트
+#host="m.damoa.io"  #이노온 테스트 사이트
 port=1883
 uploadport=2883
 from default import make_ae, ae, TOPIC_list, path, supported_sensors
 
 #bridge = 80061056 #placecode 설정을 위해 변수로 재설정
-#bridge = 32345141 #placecode 설정을 위해 변수로 재설정
+bridge = 32345141 #placecode 설정을 위해 변수로 재설정
 #bridge = 80062056 #placecode 설정을 위해 변수로 재설정
-bridge = 11110000
+#bridge = 11110000
 
 install= {"date":"2022-04-25","place":"금남2교(하)","placecode":F"{bridge}","location":"6.7m(P2~P3)","section":"최우측 거더","latitude":"37.657248","longitude":"127.359962","aetype":"D"}
 connect={"cseip":host,"cseport":7579,"csename":csename,"cseid":csename,"mqttip":host,"mqttport":port,"uploadip":host,"uploadport":uploadport}
 
 # AC X,Y,Z can't coexist in current conf
 make_ae(F'ae.{bridge}-AC_S1M_01_X', csename, install, connect)
-make_ae(F'ae.{bridge}-DI_S1M_01_X', csename, install, connect)
+#make_ae(F'ae.{bridge}-DI_S1M_01_X', csename, install, connect)
 #make_ae(F'ae.{bridge}-TP_S1M_01_X', csename, install, connect)
-make_ae(F'ae.{bridge}-TI_S1M_01_X', csename, install, connect)
+#make_ae(F'ae.{bridge}-TI_S1M_01_X', csename, install, connect)
 
 root='/home/pi/GB'
 if os.path.exists(F"{root}/config.dat"): 
